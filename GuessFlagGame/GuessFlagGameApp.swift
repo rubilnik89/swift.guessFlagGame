67ee9791b3d9eaf88bd75c2d834e82b8f0b90b4c
Roman Zherebko <romanzherebko@Mac-mini-Roman.local>
2022-05-20 15:10:58 +0300

    init

---
+++ GuessFlagGame/GuessFlagGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessFlagGameApp.swift
+//  GuessFlagGame
+//
+//  Created by Roman Zherebko on 20.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessFlagGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
